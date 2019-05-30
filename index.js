e13d00867db42d8001dba447a370b28ff2292e5a
Erica Titkemeyer <ekemeyer@gmail.com>
2019-05-30 23:20:21 +0000

    added pluralize npm

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var pluralize = require('pluralize')
+
 var words = ['dog', 'person', 'moose', 'ox', 'wife', 'guest', 'man', 'woman', 'mess', 'photo', 'avocado', 'superhero', 'bar', 'monkey', 'combo', 'toy', 'chair', 'toe'];
 
 function getPluralization(word) {
