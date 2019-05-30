716f00ffc756db930d8c2f7204e482df8af73688
Erica Titkemeyer <ekemeyer@gmail.com>
2019-05-30 23:45:05 +0000

    included pluralize in function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var pluralize = require('pluralize')
+var pluralize = require('pluralize');
 
 var words = ['dog', 'person', 'moose', 'ox', 'wife', 'guest', 'man', 'woman', 'mess', 'photo', 'avocado', 'superhero', 'bar', 'monkey', 'combo', 'toy', 'chair', 'toe'];
 
 function getPluralization(word) {
-    // This needs to be replaced with the code that returns a pluralized version of the input word
-    return word;
+    return pluralize(word);
 }
 
 function printPluralizedWord(word) {
